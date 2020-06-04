45ca22c20fa765432ac69754df4ed53bc36cbbd4
EriclesRibeiro <ericles00@hotmail.com>
2020-06-04 17:20:48 -0300

    Signed-off-by: EriclesRibeiro <ericles00@hotmail.com>

---
+++ Besker_Project - FIXO/Besker_Project/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Besker_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Limpar_Box1(object sender, EventArgs e)
+        {
+            if (emailText.Text == "Email") {
+                emailText.Clear();
+            }
+            
+        }
+        private void Limpar_Box2(object sender, EventArgs e)
+        {
+            if (senhaText.Text == "Senha") {
+                senhaText.Clear();
+            }
+        }
+
+        private void senhaText_TextChanged(object sender, EventArgs e)
+        {
+            senhaText.PasswordChar = '*' ;
+        }
+
+        private void Recuperar_Senha(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
